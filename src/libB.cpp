7117aac76dff3d255ac93b7e22d8affa9d66e477
javierg <javierg@jfrog.com>
2019-11-15 13:31:28 +0100

    initial

--- src/libB.cpp
+++ src/libB.cpp
@@ -5,5 +5,6 @@
 #include "libA/libA.h"
 
 void hello_libB(int indent, const std::string& msg) {
-    std::cout << std::string(indent, ' ') << msg << std::endl;
+    std::cout << std::string(indent, ' ') << "libB: " << msg << std::endl;
+    hello_libA(indent+1, "called from libB");
 }
